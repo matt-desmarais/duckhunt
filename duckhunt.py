85a08b64dea687cd4a899b3870f59d65e491f423
Matt <matt.desmarais78@gmail.com>
2016-05-11 17:41:17 -0400

    Update duckhunt.py

--- duckhunt.py
+++ duckhunt.py
@@ -22,6 +22,105 @@ TITLE = "Symons Media: Duck Hunt"
 FRAMES_PER_SEC = 50
 BG_COLOR = 255, 255, 255
 
+def writeACC(register,value):
+        bus.write_byte_data(ACC_ADDRESS , register, value)
+        return -1
+
+def writeMAG(register,value):
+        bus.write_byte_data(MAG_ADDRESS, register, value)
+        return -1
+
+def writeGRY(register,value):
+        bus.write_byte_data(GYR_ADDRESS, register, value)
+        return -1
+
+
+
+def readACCx():
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_X_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_X_H_A)
+	acc_combined = (acc_l | acc_h <<8)
+
+	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
+
+def readACCy():
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Y_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Y_H_A)
+	acc_combined = (acc_l | acc_h <<8)
+
+	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
+
+def readACCz():
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Z_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Z_H_A)
+	acc_combined = (acc_l | acc_h <<8)
+
+	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
+
+def readMAGx():
+        mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_X_L_M)
+        mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_X_H_M)
+        mag_combined = (mag_l | mag_h <<8)
+
+        return mag_combined  if mag_combined < 32768 else mag_combined - 65536
+
+
+def readMAGy():
+        mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_Y_L_M)
+        mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_Y_H_M)
+        mag_combined = (mag_l | mag_h <<8)
+
+        return mag_combined  if mag_combined < 32768 else mag_combined - 65536
+
+
+def readMAGz():
+        mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_Z_L_M)
+        mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_Z_H_M)
+        mag_combined = (mag_l | mag_h <<8)
+
+        return mag_combined  if mag_combined < 32768 else mag_combined - 65536
+
+
+
+def readGYRx():
+        gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_X_L_G)
+        gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_X_H_G)
+        gyr_combined = (gyr_l | gyr_h <<8)
+
+        return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
+  
+
+def readGYRy():
+        gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_Y_L_G)
+        gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_Y_H_G)
+        gyr_combined = (gyr_l | gyr_h <<8)
+
+        return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
+
+def readGYRz():
+        gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_Z_L_G)
+        gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_Z_H_G)
+        gyr_combined = (gyr_l | gyr_h <<8)
+
+        return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
+
+#initialise the accelerometer
+writeACC(CTRL_REG1_XM, 0b01100111) #z,y,x axis enabled, continuos update,  100Hz data rate
+writeACC(CTRL_REG2_XM, 0b00100000) #+/- 16G full scale
+
+#initialise the magnetometer
+writeMAG(CTRL_REG5_XM, 0b11110000) #Temp enable, M data rate = 50Hz
+writeMAG(CTRL_REG6_XM, 0b01100000) #+/-12gauss
+writeMAG(CTRL_REG7_XM, 0b00000000) #Continuous-conversion mode
+
+#initialise the gyroscope
+writeGRY(CTRL_REG1_G, 0b00001111) #Normal power mode, all axes enabled
+writeGRY(CTRL_REG4_G, 0b00110000) #Continuos update, 2000 dps full scale
+
+
 # Initialize pygame before importing modules
 pygame.mixer.pre_init(44100, -16, 2, 1024)
 pygame.init()
@@ -78,6 +177,7 @@ class Game(object):
                 self.handleEvent(event)
             self.loop()
             self.render()
+             pyautogui.moveTo(ACCx, ACCy)
             if(GPIO.input(buttonPin))
                 pyautogui.click()
         self.cleanup()
