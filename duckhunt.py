de8508fa73d745226b1afa8f82b927c7093dd101
Matt <matt.desmarais78@gmail.com>
2016-05-13 17:42:10 -0400

    Update duckhunt.py

--- duckhunt.py
+++ duckhunt.py
@@ -2,12 +2,6 @@ import os, sys
 import pygame
 import pygame.transform
 
-import smbus
-import time
-import math
-from LSM9DS0 import *
-import datetime
-bus = smbus.SMBus(1)
 
 import RPi.GPIO as GPIO
 import time
@@ -22,47 +16,7 @@ TITLE = "Symons Media: Duck Hunt"
 FRAMES_PER_SEC = 50
 BG_COLOR = 255, 255, 255
 
-def writeACC(register,value):
-        bus.write_byte_data(ACC_ADDRESS , register, value)
-        return -1
 
-def writeMAG(register,value):
-        bus.write_byte_data(MAG_ADDRESS, register, value)
-        return -1
-
-def writeGRY(register,value):
-        bus.write_byte_data(GYR_ADDRESS, register, value)
-        return -1
-
-
-
-def readACCx():
-        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_X_L_A)
-        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_X_H_A)
-	acc_combined = (acc_l | acc_h <<8)
-
-	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
-
-
-def readACCy():
-        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Y_L_A)
-        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Y_H_A)
-	acc_combined = (acc_l | acc_h <<8)
-
-	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
-
-
-def readACCz():
-        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Z_L_A)
-        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Z_H_A)
-	acc_combined = (acc_l | acc_h <<8)
-
-	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
-
-
-#initialise the accelerometer
-writeACC(CTRL_REG1_XM, 0b01100111) #z,y,x axis enabled, continuos update,  100Hz data rate
-writeACC(CTRL_REG2_XM, 0b00100000) #+/- 16G full scale
 
 
 # Initialize pygame before importing modules
@@ -119,22 +73,12 @@ class Game(object):
         self.init()
 
         while (self.running):
-            oldx = readACCx()
-            oldy = readACCy()
+
             for event in pygame.event.get():
                 self.handleEvent(event)
             self.loop()
             self.render()
-            newx = readACCx()
-            newy = readACCy()
-            if(oldx>newx):
-            	pyautogui.moveRel(-10,0)
-            elif(oldx<newx):
-            	pyautogui.moveRel(10,0)
-            if(oldy>newy):
-            	pyautogui.moveRel(0,-10)
-            elif(oldy<newy):
-            	pyautogui.moveRel(0,10)
+      
             
             if(GPIO.input(buttonPin)):
                 pyautogui.click()
